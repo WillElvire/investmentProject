01463807401184cd13934893fe6271d97481615c
WillElivre <willymarley76@gmail.com>
2020-08-01 12:07:18 +0000

    add all

---
+++ public/js/rsi.js
@@ -0,0 +1,11 @@
+function augmentation(){
+
+var montant=document.getElementById('invest');
+var received=document.getElementById('received');
+var rsi =document.getElementById('RSI');
+
+console.log(rsi.value);
+
+
+
+}
